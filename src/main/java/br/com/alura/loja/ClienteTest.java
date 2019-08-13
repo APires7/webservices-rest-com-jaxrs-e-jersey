2f6801ecbe27812d6be68d4a18606acdfce973b5
Allan Victor Pires <allanpires.a7@gmail.com>
2019-08-13 14:37:40 -0300

    Inclusao de classe Servidor e deserializacao de objetos

--- src/main/java/br/com/alura/loja/ClienteTest.java
+++ src/main/java/br/com/alura/loja/ClienteTest.java
@@ -4,22 +4,60 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Projeto;
 import junit.framework.Assert;
 
 public class ClienteTest {
 
+    private HttpServer server;
+
+    @Before
+    public void startServidor() {
+        this.server = new Servidor().inicializaServidor();
+    }
+
+    @After
+    public void stopServidor() {
+        this.server.stop();
+    }
+
     @Test
-    public void testaQueAConexaoComOServidorFunciona() {
+    public void testaURICarrinho() {
         Client client = ClientBuilder.newClient();
 
         // URI base do servidor para fazer request
-        WebTarget target = client.target("http://www.mocky.io");
-        
-        String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
-        
-        Assert.assertTrue(conteudo.contains("<rua>Rua Vergueiro 3185"));
+        WebTarget target = client.target("http://localhost:8080");
+
+        String conteudo = target.path("/carrinhos").request().get(String.class);
+
+        // Deserializa o objeto
+        Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+        Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+
+        // Sem deserializar o objeto
+        // Assert.assertTrue(conteudo.contains("<rua> Rua Vergueiro 3185"));
+    }
+
+    @Test
+    public void testaURIProjeto() {
+        Client client = ClientBuilder.newClient();
+
+        // URI base do servidor para fazer request
+        WebTarget target = client.target("http://localhost:8080");
+
+        String conteudo = target.path("/projetos").request().get(String.class);
+
+        // Deserializa o objeto
+        Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+        Assert.assertEquals("Minha loja", projeto.getNome());
     }
 
 }
