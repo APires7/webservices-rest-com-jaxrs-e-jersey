64acd443c35c5eabee5f17c731bd765bd415a7b6
Allan Victor Pires <allan_ve_pires@hotmail.com>
2019-08-26 21:55:52 -0300

    Ajustes no testes para inclusao de status code

--- src/main/java/br/com/alura/loja/ClienteTest.java
+++ src/main/java/br/com/alura/loja/ClienteTest.java
@@ -83,7 +83,10 @@ public class ClienteTest {
 		Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
 
 		Response response = target.path("/carrinhos").request().post(entity);
-		Assert.assertEquals("<status>Sucesso</status>", response.readEntity(String.class));
+		Assert.assertEquals(201, response.getStatus());
+		String location = response.getHeaderString("Location");
+		String conteudo = client.target(location).request().get(String.class);
+		Assert.assertTrue(conteudo.contains("Tablet"));
 	}
 
 }
